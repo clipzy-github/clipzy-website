5dca19d9b1579aadbe23db79e6f9b0d78fbdec11
Jose Moran Urena <jose@josemoranurena.tech>
2024-08-23 18:05:35 -0400

    delete unnecessary

--- simplechatapp/functions/chat.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event, context) => {
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Hello from the chat function!' })
-    };
-    return response;
-  };
-  
